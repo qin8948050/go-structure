b331534e83cda408b6d3721ac67792216d7b2237
xwqin <xwqin@gaojihealth.com>
2021-07-23 09:36:11 +0800

    afe

---
+++ 简单/剑指offer/剑指 Offer 55 - I. 二叉树的深度.go
@@ -0,0 +1,38 @@
+/*
+
+输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+
+例如：
+
+给定二叉树 [3,9,20,null,null,15,7]，
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3  
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	var left int
+	var right int
+	if root ==nil{
+		return 0
+	} else {
+		left=maxDepth(root.Left)
+		right=maxDepth(root.Right)
+	}
+	if left<right{
+		return right+1
+	} else{
+		return left+1
+	}
+}
