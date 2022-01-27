fa092f637533be55d28a28bcabd1b3a101c5c868
xwqin <xwqin@gaojihealth.com>
2022-01-27 11:14:33 +0800

    超时控制

--- tmp/main.go
+++ tmp/main.go
@@ -4,39 +4,38 @@ import (
 	"fmt"
 )
 
-func main()  {
-	l1:=[]int{1,2,3,4,5}
-	result:=BinarySearch(l1,5)
+func main() {
+	l1 := []int{4, 1, 2, 3, 5}
+	result := BubbleSort(l1)
 	fmt.Println(result)
 }
 
-
-func BubbleSort(list []int) []int{
-	for i:=0;i<=len(list)-1;i++{
-		for j:=i;j<=len(list)-1;j++{
-			if list[i]>list[j]{
-				tmp:=list[i]
-				list[i]=list[j]
-				list[j]=tmp
+func BubbleSort(l []int) []int {
+	for i := 0; i <= len(l)-1; i++ {
+		for j := 0; j < len(l)-i-1; j++ {
+			if l[j] < l[j+1] {
+				tmp := l[j]
+				l[j] = l[j+1]
+				l[j+1] = tmp
 			}
 		}
 	}
-	return list
+	return l
 }
 
-func BinarySearch(list []int,searchValue int) int{
-	low:=0
-	high:=len(list)-1
+func BinarySearch(list []int, searchValue int) int {
+	low := 0
+	high := len(list) - 1
 	var mid int
-	for low<=high{
-		mid:=low+(high-low)/2
-		if list[mid]==searchValue{
+	for low <= high {
+		mid := low + (high-low)/2
+		if list[mid] == searchValue {
 			return mid
-		} else if list[mid]>searchValue{
-			high=mid-1
+		} else if list[mid] > searchValue {
+			high = mid - 1
 		} else {
-			low=mid+1
-		} 
+			low = mid + 1
+		}
 	}
 	return mid
 }
---
+++ 并发控制/超时控制.go
@@ -0,0 +1,62 @@
+//方法一 time.NewTicker
+func method1(){
+	close:=make(chan int)
+	cleanup:=time.NewTicker(2*time.Second)
+	defer cleanup.Stop()
+	go func(){
+		a:=func () int {
+			return 1
+		}()
+		time.Sleep(3*time.Second)
+		close<-a
+	}()
+	select {
+		case result:=<-close:
+			fmt.Println(result)
+		case <-cleanup.C:
+			fmt.Println("timeout")
+	}
+}
+
+
+//方法二 context.WithTimeout
+func method1(){
+	ctx,cancel:=context.WithTimeout(context.Background(),2*time.Second)
+	defer cancel()
+	//为了防止超时后导致goroutine不受控制内存泄漏情况
+	c := make(chan int,1)   
+	go func() {
+		time.Sleep(3*time.Second)
+		c <- 1
+	}()
+	select {
+	case <-ctx.Done():
+		fmt.Println("Timeout")
+	case res := <-c:
+		fmt.Println("aaaa",res)
+	}
+}
+
+//扩展
+func job(){
+	ctx,cancel:=context.WithTimeout(context.Background(),2*time.Second)
+	defer cancel()
+	c := make(chan int,0)
+	go func(ctx context.Context) {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case c<-rpc():
+			}
+		}
+
+
+	}(ctx)
+	select {
+	case <-ctx.Done():
+		fmt.Println("Timeout")
+	case res := <-c:
+		fmt.Println("aaaa",res)
+	}
+}
